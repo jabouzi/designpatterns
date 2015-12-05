955b6d92704f2dcdf0e517e5f66003e4afa98089
Skander Jabouzi <jabouzi@gmail.com>
2015-12-05 14:49:33 -0500

    add Adapter DP

---
+++ Structural/Adapter/AceClass.java
@@ -0,0 +1,14 @@
+public class AceClass implements AceInterface 
+{
+  String name;
+
+  public void setName(String n)
+  {
+    name = n;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+}
