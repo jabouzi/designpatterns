202691237947404b56484536590cb5907b114e55
Skander Jabouzi <jabouzi@gmail.com>
2015-12-07 21:14:46 -0500

    add facade folder

---
+++ Structural/Facade/TestFacade.java
@@ -0,0 +1,19 @@
+public class TestFacade 
+{
+  public static void main(String args[])
+  {
+    TestFacade t = new TestFacade();
+  }
+
+  public TestFacade()
+  {
+
+    SimpleProductFacade simpleProductFacade = 
+      new SimpleProductFacade();
+
+    simpleProductFacade.setName("printer");
+
+    System.out.println("The product is a " + 
+      simpleProductFacade.getName());
+  }
+}
